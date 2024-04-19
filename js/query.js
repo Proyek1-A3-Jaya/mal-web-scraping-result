35a3205562153c40f6fed8a90ed6ea28526523c0
WeirdoKitten <satriapermata12@gmail.com>
2024-04-19 23:33:41 +0700

    "mengganti layout trending"

--- js/query.js
+++ js/query.js
@@ -1,5 +1,5 @@
 // Ambil data dari hasil.json
-fetch('manga_data.json')
+fetch('/assets/manga_data.json')
     .then(response => response.json())
     .then(data => {
         // Panggil fungsi untuk mengisi data ke template
